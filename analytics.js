c40efd3f4aa919d0e41bc63537093cedf61a09dc
Song Yangyu <flyfy1@gmail.com>
2026-09-15 00:18:12 +0800

    Publish Things 4b372b94f469

--- analytics.js
+++ analytics.js
@@ -7,13 +7,16 @@
   window.dataLayer = window.dataLayer || [];
   function gtag() { window.dataLayer.push(arguments); }
   gtag('js', new Date());
+  const page = { page_location: `https://${location.hostname}${path}`, page_title: 'Integ Things', page_referrer: '' };
   gtag('config', 'G-3Q4M53BMJQ', {
+    send_page_view: false,
     page_location: `https://things.integ.life${path}`,
     page_title: 'Integ Things',
     page_referrer: '',
     allow_google_signals: false,
     allow_ad_personalization_signals: false,
   });
+  gtag('event', 'page_view', { ...page, send_to: 'G-3Q4M53BMJQ' });
   const script = document.createElement('script');
   script.async = true;
   script.src = 'https://www.googletagmanager.com/gtag/js?id=G-3Q4M53BMJQ';
